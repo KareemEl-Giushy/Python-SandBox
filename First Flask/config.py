497eac646915752b55401a9088025f5a9001123c
Kareem El-Giushy <kareem.64123@gmail.com>
2021-03-09 19:11:17 +0200

    Making The Structure Jinja templates and more

--- First Flask/config.py
+++ First Flask/config.py
@@ -3,17 +3,21 @@
 # Config File
 # -------------
 # =========================
-from flask import Flask
+from flask import Flask, render_template
 
 skills_app = Flask(__name__)
 
 @skills_app.route("/")
 def indexApp():
-    return "Hello From Flask Config"
+    return render_template("index.html", mytitle="First Flask App Hamada", testy="Hello A")
 
 @skills_app.route("/about")
 def aboutApp():
-    return "Hello From Flask This is About -Eng.Kareem"
+    return render_template("about.html", mytitle="First Flask App Hamada", testy="Hello B")
+@skills_app.route("/add")
+def addApp():
+    return render_template("add.html", mytitle="add From Flask")
+
 
 if __name__ == "__main__":
     skills_app.run(debug=True, port=9000)
