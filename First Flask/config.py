460992f5bd2960f60e082d17df551ba48cf2ab44
Kareem El-Giushy <kareem.64123@gmail.com>
2021-03-10 14:04:27 +0200

    Make changes ! change is great !

--- First Flask/config.py
+++ First Flask/config.py
@@ -9,15 +9,21 @@ skills_app = Flask(__name__)
 
 @skills_app.route("/")
 def indexApp():
-    return render_template("index.html", mytitle="First Flask App Hamada", testy="Hello A")
+    return render_template("index.html", mytitle="First Flask App Hamada")
 
 @skills_app.route("/about")
 def aboutApp():
-    return render_template("about.html", mytitle="First Flask App Hamada", testy="Hello B")
+    return render_template("about.html", mytitle="First Flask App Hamada")
 @skills_app.route("/add")
 def addApp():
-    return render_template("add.html", mytitle="add From Flask")
+    return render_template("add.html", mytitle="add From Flask", cssfiles=['add.css'])
 
+@skills_app.route("/skills")
+def skills():
+    return render_template("skills.html",
+                            mytitle="My Skills",
+                            page_head="These are my skills",
+                            discription="This is my skills page", cssfiles=['skills.css'], skills=[("html", 90), ("css", 90), ('php', 80), ('python', 70)])
 
 if __name__ == "__main__":
     skills_app.run(debug=True, port=9000)
